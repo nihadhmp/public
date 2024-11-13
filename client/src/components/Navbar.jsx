08a7bf5eccfde2d7b456fdcd9745ff57bd1d2e35
Nihadh MP <nihadh068@gmail.com>
2024-11-13 15:30:40 +0530

    added side bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,19 @@
 import { Link } from "react-router-dom";
 import Countrylist from "./Countrylist";
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaRegWindowClose } from "react-icons/fa";
+import { useState } from "react";
 
 function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
+
+  const openSidebar = () => {
+    setSidebar(true);
+  };
+
+  const closeSidebar = () => {
+    setSidebar(false);
+  };
+
   return (
     <>
       <nav className=" bg-teal-900 px-3 lg:px-36 py-3 ">
@@ -25,10 +36,41 @@ function Navbar() {
           <Countrylist />
         </div>
         <div className="lg:hidden flex justify-between ">
-          <FaBars className="text-slate-300 items-start size-7 " />
+          <FaBars
+            onClick={openSidebar}
+            className="text-slate-300 items-start size-7 "
+          />
           <Countrylist />
         </div>
       </nav>
+
+      {sidebar && (
+        <nav className="bg-teal-950 w-2/3 h-screen p-3 absolute">
+          <section className="flex justify-between ">
+            <ul className="text-xl font-semibold flex flex-col justify-between gap-y-10  text-slate-300 ">
+              <li className="hover:text-white">
+                <Link to={"/"}>HOME</Link>
+              </li>
+              <li className="hover:text-white">
+                <Link to={"/news"}>NEWS</Link>
+              </li>
+              <li className="hover:text-white">
+                {" "}
+                <Link to={"/travel"}>TRAVEL</Link>
+              </li>
+              <li className="hover:text-white">
+                <Link to={"/hotels"}>HOTELS</Link>
+              </li>
+            </ul>
+            <div>
+              <FaRegWindowClose
+                className="size-8 text-slate-300 hover:text-white"
+                onClick={closeSidebar}
+              />
+            </div>
+          </section>
+        </nav>
+      )}
     </>
   );
 }
